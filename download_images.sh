4f598810048de71b0601916c99a23df040a2b628
Skai222 <kirill.wolf@icloud.com>
2026-03-11 19:18:40 +0100

    Create download_images.sh

---
+++ download_images.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir -p images && cd images
+curl -sL -o 01_ccf_training.jpg "https://res.cloudinary.com/da1molee1/image/upload/c_fill,g_center,h_439,w_780/q_auto:eco/chintuck-sideview-tuckwithnohand-color.jpeg?_a=BAAAV6E0"
+curl -sL -o 02_haltungskorrektur.jpg "https://res.cloudinary.com/da1molee1/image/upload/c_fill,g_center,h_439,w_780/q_auto:eco/easystretchesneckshoulderpain-24.jpeg?_a=BAAAV6E0"
+curl -sL -o 03_scm_selbstbehandlung.jpg "https://www.healthlinkbc.ca/sites/default/files/healthwise/images/h9991327_002.jpg"
+curl -sL -o 04_isometrische_rotation.jpg "https://dbmmu5j6lxew.cloudfront.net/pe-exercise_neckrotation.jpg"
+curl -sL -o 05_scm_dehnung.jpg "https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/topic_centers/Fitness-Exercise/642x361_Stretches_to_Loosen_Your_Trapezius_Muscles-Ear_to_Shoulder.jpg?w=1155&h=758"
+curl -sL -o 06_skapula.jpg "https://images.ctfassets.net/hjcv6wdwxsdz/6zmD8jI4jQhNCLLYqJvJ9r/33e963f567d0e9d016eb029115875c8f/scapular-squeeze-carole-rest.png?w=800&h=588&q=90&fm=png"
+curl -sL -o 07_extensor_vierfuessler.jpg "https://myrehabconnection.com/wp-content/uploads/2017/02/Neck-Stability-Exercises_Basic-Supine-Neck-Retraction-1.png"
+curl -sL -o 08_subokzipital.jpg "https://www.yaharatherapy.com/wp-content/uploads/2020/01/YT1.jpg"
+curl -sL -o 09_kiefer_mundoeffnung.jpg "https://myrangemaster.com/wp-content/uploads/2021/12/q3egb7cejkhv95dak8u8rqtai20o.jpg"
+curl -sL -o 10_kiefer_widerstand.jpg "https://myrangemaster.com/wp-content/uploads/2021/12/q3egb7cejkhv95dak8u8rqtai20o.jpg"
+curl -sL -o 11_bws_extension.jpg "https://myrangemaster.com/wp-content/uploads/2021/12/q3egb7cejkhv95dak8u8rqtai20o.jpg"
+echo "Fertig!"
